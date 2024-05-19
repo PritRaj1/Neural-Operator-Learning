004922d73c5b6145756fd820b7fb262f9fdddeea
Prithvi <pr478@cam.ac.uk>
2024-05-19 09:48:14 +0100

    rename

--- train_darcy.jl
+++ train_darcy.jl
@@ -11,7 +11,10 @@ using Flux
 using Optimisers
 using ConfParser
 
-conf = ConfParse("CNN_config.ini")
+MODEL_NAME = "CNN"
+
+# Parse config
+conf = ConfParse("$MODEL_NAME_config.ini")
 parse_conf!(conf)
 batch_size = parse(Int, retrieve(conf, "DataLoader", "batch_size"))
 num_epochs = parse(Int, retrieve(conf, "Pipeline", "num_epochs"))
@@ -29,17 +32,24 @@ if !isdir("logs")
     mkdir("logs")
 end
 
-# Create log file
-open("logs/CNN.csv", "w") do file
-    write(file, "epoch,train_loss,test_loss\n")
+# Create log file if it doesn't exist
+if !isfile("logs/$MODEL_NAME.csv")
+    open("logs/$MODEL_NAME.csv", "w") do file
+        write(file, "epoch,train_loss,test_loss\n")
+    end
 end
+
+# Create trained_models directory if it doesn't exist
+if !isdir("trained_models")
+    mkdir("trained_models")
+end 
 
 # Train the model
 opt_state = Dict(
     "adam" => Optimisers.setup(Optimisers.Adam(LR), model),
     "sgd" => Optimisers.setup(Optimisers.Descent(LR), model)
 )[optimizer_type]
-model = train_model(model, train_loader, test_loader, opt_state, loss_fcn, num_epochs, "CNN")
+model = train_model(model, train_loader, test_loader, opt_state, loss_fcn, num_epochs, MODEL_NAME)
 
 # Save the model
-Flux.save("trained_models/CNN_model.bson", model)
+Flux.save("trained_models/$MODEL_NAME.bson", model)
